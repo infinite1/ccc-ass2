60d3b72a9557b9778b9f2e53bb4f784636426fa3
camizous <49180472+camizous@users.noreply.github.com>
2020-05-08 18:06:17 +1000

    upload crawler

---
+++ crawler/tweepy_job.py
@@ -0,0 +1,81 @@
+from tweepy import API 
+from tweepy import Cursor
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+ 
+import twitter_credentials
+import json
+import time
+
+# # # # TWITTER AUTHENTICATER # # # #
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+
+# # # # TWITTER STREAMER # # # #
+class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+    def __init__(self):
+        self.twitter_autenticator = TwitterAuthenticator()
+        self.error_num = 0    
+
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_autenticator.authenticate_twitter_app() 
+        stream = Stream(auth, listener)
+        # stream.filter(track=hash_tag_list)
+        while True:
+            try:
+                stream.filter(locations=[112.15, -44.84 ,155.65, -11])
+                break
+            except Exception as e:
+                print(e)
+                self.error_num += 1
+                print("ERROR---Sleeping", self.error_num)
+                time.sleep(60)
+
+
+# # # # TWITTER STREAM LISTENER # # # #
+class TwitterListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+        self.count = 0
+
+    def on_data(self, data):
+        try:
+            tweet = json.loads(data)
+            location = tweet["user"]["location"]
+            country = tweet["place"]["country"]
+            print(country, self.count)
+            self.count +=1
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+          
+    def on_error(self, status):
+        if status == 420:
+            # Returning False on_data method in case rate limit occurs.
+            return False
+        print(status)
+
+ 
+if __name__ == '__main__':
+ 
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["the"]
+    fetched_tweets_filename = "result_geo_aus_7.json"
+
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
