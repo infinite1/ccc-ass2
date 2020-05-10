fa842822bbe8d6148d71cf65b2974f07131c92a1
Shiyu-D <barbaradsy@gmail.com>
2020-05-11 03:44:41 +1000

    qqqq

--- nectar/couchDB.sh
+++ nectar/couchDB.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 echo "== Set variables =="
 declare -a nodes=(172.26.131.10 172.26.130.42)
 declare -a ports=(5984 15984)
@@ -11,22 +9,18 @@ export pass=pass
 
 echo "== Start the containers =="
 docker-compose up -d
+sleep 10
 
-sleep 30
-
-# localhost = 127.0.0.1 in all computer
 echo "== Enable cluster setup =="
 for (( i=0; i<${size}; i++ )); do
   curl -X POST "http://${user}:${pass}@localhost:${ports[${i}]}/_cluster_setup" -H 'Content-Type: application/json' \
     -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"${user}\", \"password\":\"${pass}\", \"node_count\":\"${size}\"}"
 done
 
-sleep 10
-
 echo "== Add nodes to cluster =="
 for (( i=0; i<${size}; i++ )); do
   if [ "${nodes[${i}]}" != "${master_node}" ]; then
-    curl -X POST -H 'Content-Type: application/json' http://${user}:${pass}@:${master_port}/_cluster_setup \
+    curl -X POST -H 'Content-Type: application/json' http://${user}:${pass}@127.0.0.1:${master_port}/_cluster_setup \
       -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"${user}\", \"password\":\"${pass}\", \"port\": 5984, \"node_count\": \"${size}\", \
            \"remote_node\": \"${nodes[${i}]}\", \"remote_current_user\": \"${user}\", \"remote_current_password\": \"${pass}\"}"
     curl -X POST -H 'Content-Type: application/json' http://${user}:${pass}@127.0.0.1:${master_port}/_cluster_setup \
@@ -34,13 +28,8 @@ for (( i=0; i<${size}; i++ )); do
   fi
 done
 
-sleep 10
-
 curl -X POST "http://${user}:${pass}@localhost:${master_port}/_cluster_setup" -H 'Content-Type: application/json' -d '{"action": "finish_cluster"}'
 
 curl http://${user}:${pass}@localhost:${master_port}/_cluster_setup
 
 for port in "${ports[@]}"; do  curl -X GET http://${user}:${pass}@localhost:${port}/_membership; done
-
-
-# curl -XPUT "http://user:pass@127.0.0.1:5984/twitter"
