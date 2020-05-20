185f469578039973207f426ca6e69ea484cacaf8
jluo93 <jluo93@student.monash.edu>
2020-05-20 13:43:17 +1000

    new icon style, add two new layer control and rebuild code to make it clean

--- demo_website/map/views.py
+++ demo_website/map/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 import folium
+from folium.plugins import BeautifyIcon
 import geopandas as geopd
 import pandas as pd
 import json
@@ -21,52 +22,30 @@ class FoliumView(TemplateView):
         results = tweet_db.find(mango)
         return results
 
-    def get_context_data(self, **kwargs):
-        #figure = folium.Figure()
-        
-        url = 'https://raw.githubusercontent.com/infinite1/ccc-ass2/master/demo_website'
-        aus_geo = f'{url}/australian-states.json'
-        aus_unemployment = f'{url}/aus_job_data.csv'
-        aus_data = pd.read_csv(aus_unemployment)
-        aus=geopd.read_file(aus_geo)
-        aus['STATE_CODE']=aus['STATE_CODE'].astype(int)
-        aus=aus.merge(aus_data,on="STATE_CODE")
-        
-        # m = folium.Map(location=[-27.81, 134.96],zoom_start=5)
-        
-        #m.add_to(figure)
-
-        figure = folium.Figure()
-        m = folium.Map(
-            location=[-27.81, 134.96],
-            zoom_start=4,
-        )
-        #add streeming tweets into map
+    def add_tweets_on_map(self):
+        fg_positive=folium.FeatureGroup(name='Positive tweets', show=False)
+        fg_negtive=folium.FeatureGroup(name='Negtive tweets', show=False)
+        # add streeming tweets into map
         tweets=self.get_tweets()
         if tweets:
             for tweet in tweets:
                 try:
-                    folium.Marker(
-                        location=[tweet['coordinates'][1],tweet['coordinates'][0]],
-                        popup=tweet['text'],
-                        icon=folium.Icon(icon='cloud')
-                    ).add_to(m)
+                    if tweet['sentiment']["polarity"]>0:
+                        folium.Marker(
+                            location=[tweet['coordinates'][1],tweet['coordinates'][0]],
+                            popup=tweet['text'],
+                            icon=BeautifyIcon(icon='thumbs-up',background_color='#99CCFF',border_color='transparent')
+                        ).add_to(fg_positive)
+                    else:
+                        folium.Marker(
+                            location=[tweet['coordinates'][1],tweet['coordinates'][0]],
+                            popup=tweet['text'],
+                            icon=BeautifyIcon(icon='thumbs-down',background_color='#FF9999',border_color='transparent')
+                        ).add_to(fg_negtive)
                 except:
                     pass
-        m.add_to(figure)
-    
-        folium.Choropleth(
-            geo_data=aus,
-            name='Unemployment in State',
-            data=aus_data,
-            columns=['STATE_CODE','Job Loss Rate'],
-            key_on="feature.properties.STATE_CODE",
-            fill_color='YlGn',
-            fill_opacity=0.7,
-            line_opacity=0.2,
-            legend_name='Unemployment Rate (%)',
-            smooth_factor=0
-        ).add_to(m)
+        return fg_positive,fg_negtive
+    def interactive_map(self,data):
         style_function = lambda x: {'fillColor': '#ffffff', 
                             'color':'#000000', 
                             'fillOpacity': 0.1, 
@@ -76,7 +55,7 @@ class FoliumView(TemplateView):
                                         'fillOpacity': 0.50, 
                                         'weight': 0.1}
         NIL = folium.features.GeoJson(
-            aus,
+            data,
             style_function=style_function, 
             control=False,
             highlight_function=highlight_function, 
@@ -86,9 +65,45 @@ class FoliumView(TemplateView):
                 style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
             )
         )
+        return NIL
+    def get_context_data(self, **kwargs):
+        url = 'https://raw.githubusercontent.com/infinite1/ccc-ass2/master/demo_website'
+        aus_geo = f'{url}/australian-states.json'
+        aus_unemployment = f'{url}/aus_job_data.csv'
+        aus_data = pd.read_csv(aus_unemployment)
+        aus=geopd.read_file(aus_geo)
+        aus['STATE_CODE']=aus['STATE_CODE'].astype(int)
+        aus=aus.merge(aus_data,on="STATE_CODE")
+
+        
+        figure = folium.Figure()
+        m = folium.Map(
+            location=[-27.81, 164.96],
+            zoom_start=4,
+        )
+        m.add_to(figure)
+        #color by job loss rate  
+        folium.Choropleth(
+            geo_data=aus,
+            name='Unemployment in State',
+            data=aus_data,
+            columns=['STATE_CODE','Job Loss Rate'],
+            key_on="feature.properties.STATE_CODE",
+            fill_color='YlGn',
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            legend_name='2018 Unemployment Rate (%)',
+            smooth_factor=0
+        ).add_to(m)
+        NIL=self.interactive_map(aus)
         m.add_child(NIL)
         m.keep_in_front(NIL)
+        #add feature group in layer control
+        fg_positive,fg_negtive=self.add_tweets_on_map()
+        m.add_child(fg_positive)
+        m.add_child(fg_negtive)
         folium.LayerControl().add_to(m)
+        
         figure.render()
         
         return {"map": figure}
